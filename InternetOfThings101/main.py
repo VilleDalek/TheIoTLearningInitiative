93c1d7eb4032de1f250dfebb0841c1265ccf3939
Dalek Bug <dalekbug@gmail.com>
2016-05-31 04:35:52 +0000

    Publish sensor data through MQTT Protocol
    Signed-off-by: Dalek Bug <dalekbug@gmail.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python
 
+import paho.mqtt.client as paho
 import psutil
 import signal
 import sys
 import time
 
+from threading import Thread
+
+def functionDataActuador():
+    print "Data Actuator"
+
 def functionDataSensor():
     netdata = psutil.net_io_counters()
     data = netdata.packets_sent + netdata.packets_recv
     return data
+
+def functionDataSensorMqttOnPublish(mosq, obj, msg):
+    print "Data Sensor Mqtt Published!"
+
+def functionDataSensorMqttPublish():
+    mqttclient = paho.Client()
+    mqttclient.on_publish = functionDataSensorMqttOnPublish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    while True:
+        data = functionDataSensor()
+        topic = "IoT101/DataSensor"
+        mqttclient.publish(topic, data)
+        time.sleep(1)
 
 def functionSignalHandler(signal, frame):
     sys.exit(0)
@@ -16,6 +35,9 @@ def functionSignalHandler(signal, frame):
 if __name__ == '__main__':
 
     signal.signal(signal.SIGINT, functionSignalHandler)
+
+    threadmqttpublish = Thread(target=functionDataSensorMqttPublish)
+    threadmqttpublish.start()
 
     while True:
         print "Hello Internet of Things 101"
